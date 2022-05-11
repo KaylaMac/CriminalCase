77cd68fc80b3011d327a42b3054d5eee677b9f47
Ericdkd <92001497+Ericdkd@users.noreply.github.com>
2022-05-11 10:03:48 -0400

    Update Bullet.cs

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,28 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
-
-	bool isClicked;
+public class Bullet : MonoBehaviour{
 	
+	bool isClicked;
 	GameObject bulletFacts;
 	GameObject bulletTwo;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         isClicked = PersistentData.Instance.GetBullet();
 	this.gameObject.SetActive(isClicked);
 	bulletFacts = GameObject.Find("BulletFacts");
 	bulletFacts.SetActive(false);
 	bulletTwo = GameObject.Find("bullet1");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    void Update(){      
     }
 
 	private void OnMouseDown(){
