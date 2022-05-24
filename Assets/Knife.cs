50d67c29c2e0155fb6b82f5ceabd9d77a59d21f1
Sawyer020 <78552376+Sawyer020@users.noreply.github.com>
2022-05-23 23:27:37 -0400

    Add files via upload

--- Assets/Knife.cs
+++ Assets/Knife.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Knife : MonoBehaviour
 {
     GameObject FactsAboutIsider;
-    GameObject RashawnBrazellFact;
 	bool isClicked;
     // Start is called before the first frame update
     void Start()
@@ -14,15 +13,12 @@ public class Knife : MonoBehaviour
 	this.gameObject.SetActive(isClicked);
     FactsAboutIsider = GameObject.Find("FactsAboutIsider");
 	FactsAboutIsider.SetActive(false);
-    RashawnBrazellFact = GameObject.Find("RashawnBrazellFact");
-    RashawnBrazellFact.SetActive(false);
     }
 
     private void OnMouseDown(){
 	PersistentData.Instance.SetKnifeShown();
 	PersistentData.Instance.SetLabKnife();
     FactsAboutIsider.SetActive(true);
-    RashawnBrazellFact.SetActive(true);
 	this.gameObject.SetActive(false);
     }
 }
